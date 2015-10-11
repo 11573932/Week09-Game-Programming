c2bb26f34470d03ee527150a4e0e81f1337214fd
11573932 <39087585@qq.com>
2015-10-11 23:25:15 +1000

    test commit2

--- Lab05/Game1/Game1/BasicModel.cs
+++ Lab05/Game1/Game1/BasicModel.cs
@@ -9,6 +9,7 @@ namespace Game1
     class BasicModel
     {
         // test commit
+        //test commit 2
         public Model model { get; protected set; }
        public Matrix world = Matrix.Identity;
         public BasicModel(Model model) {
